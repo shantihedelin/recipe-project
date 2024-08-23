34053a8579a6ebb4cc50c4cc0cebc5a24c61f1ca
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-08-23 10:46:55 +0200

    Co-authored-by: EmelieJohanna <EmelieJohanna@users.noreply.github.com> Co-authored-by: AdamFagerberg <AdamFagerberg@users.noreply.github.com> Co-authored-by: Petra Paulin <paulinpetra@users.noreply.github.com>

---
+++ code.js
@@ -0,0 +1,19 @@
+// - Adam: Skapa och visa upp recepten i er applikation
+
+// - Emelie: Visa upp "error" till användaren om användaren missat fylla i något i formuläret.
+
+// - Done? En bild på maträtten (inte viktigt vilken bild eller att det är en exakt bild på Bolognese utan en bild på pasta är ok, men alla recept ska ha en unik bild, receptet ska inte ha en bild som ett annat recept har)
+
+// - Petra: Betygsättning i applikationen (kan vara till exempel att man ska kunna trycka på en knapp så ökas en siffra kopplat till det receptet)
+
+// - Emelie: Kunna radera ett recept så det inte syns längre i applikationen.
+
+// - Shanti: Kunna ändra/redigera ett recept
+
+// - Shanti: localstorage - behåll recepten på sidan ???
+
+// Extra >>>>>>>>>>>>>>> Uttråkad Emelie
+// söka efter recept
+// användarregistrering
+// inloggning
+// kategorisering
